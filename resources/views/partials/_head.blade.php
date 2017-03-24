81131f881660de1ab5f1eec096f4be803fd3971e
Mengzhen <gao.mengzhen94@gmail.com>
2017-03-24 14:23:32 -0400

    insert post done

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,5 +1,4 @@
 
-  <head>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,4 +18,3 @@
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
-  </head>
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 Route::get('/contact', "PagesController@getContact");
-
 Route::get('/about', "PagesController@getAbout");
-
 Route::get('/', "PagesController@getIndex");
+
+Route::resource('posts', "PostController");
