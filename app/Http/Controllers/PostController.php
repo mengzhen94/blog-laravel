f02e04634c1519f1405a74ef23895beb71931a03
Mengzhen <gao.mengzhen94@gmail.com>
2017-03-24 16:00:03 -0400

    show one post done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -65,7 +65,8 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        return view('posts.show');
+        $post = Post::find($id);
+        return view('posts.show')->withPost($post);
     }
 
     /**
