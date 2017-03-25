872c3530f0ff86618e878c18da6d4dfa173f08d7
Mengzhen <gao.mengzhen94@gmail.com>
2017-03-24 22:21:00 -0400

    edit post --> put method done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,11 +1,10 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 use App\Post;
 use Session;
-
 class PostController extends Controller
 {
     /**
@@ -16,9 +15,8 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
-        return view('posts.index')->withPosts($posts);   
+        return view('posts.index')->withPosts($posts);
     }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -28,7 +26,6 @@ class PostController extends Controller
     {
         return view('posts.create');
     }
-
     /**
      * Store a newly created resource in storage.
      *
@@ -37,27 +34,19 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        // validate the date
+        // validate the data
         $this->validate($request, array(
-            'title' => 'required|max:255',
-            'body' => 'required'
-        ));
-
-        // store in database
+                'title' => 'required|max:255',
+                'body'  => 'required'
+            ));
+        // store in the database
         $post = new Post;
-
         $post->title = $request->title;
         $post->body = $request->body;
-
         $post->save();
-
-        Session::flash('success', 'The blog was successfully saved!');
-
-        // redirect to another page
+        Session::flash('success', 'The blog post was successfully save!');
         return redirect()->route('posts.show', $post->id);
-
     }
-
     /**
      * Display the specified resource.
      *
@@ -69,7 +58,6 @@ class PostController extends Controller
         $post = Post::find($id);
         return view('posts.show')->withPost($post);
     }
-
     /**
      * Show the form for editing the specified resource.
      *
@@ -78,10 +66,11 @@ class PostController extends Controller
      */
     public function edit($id)
     {
+        // find the post in the database and save as a var
         $post = Post::find($id);
+        // return the view and pass in the var we previously created
         return view('posts.edit')->withPost($post);
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -91,9 +80,21 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // Validate the data
+        $this->validate($request, array(
+                'title' => 'required|max:255',
+                'body'  => 'required'
+            ));
+        // Save the data to the database
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+        // set flash data with success message
+        Session::flash('success', 'This post was successfully saved.');
+        // redirect with flash data to posts.show
+        return redirect()->route('posts.show', $post->id);
     }
-
     /**
      * Remove the specified resource from storage.
      *
