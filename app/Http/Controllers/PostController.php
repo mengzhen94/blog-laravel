4714b334ef2f57f255a5b23662bb615d6f4f0eb4
Mengzhen <gao.mengzhen94@gmail.com>
2017-03-28 17:08:03 -0400

    purifier done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -65,8 +65,8 @@ class PostController extends Controller
         $post->title = $request->title;
         $post->slug = $request->slug;
         $post->category_id = $request->category_id;
-        //$post->body = Purifier::clean($request->body);
-        $post->body = $request->body;
+        $post->body = Purifier::clean($request->body);
+        //$post->body = $request->body;
 
         if ($request->hasFile('featured_img')) {
           $image = $request->file('featured_img');
@@ -147,7 +147,7 @@ class PostController extends Controller
         $post->slug = $request->input('slug');
         $post->category_id = $request->input('category_id');
         $post->body = Purifier::clean($request->input('body'));
-        $post->body = $request->input('body');
+        //$post->body = $request->input('body');
 
         if ($request->hasFile('featured_img')) {
             $image = $request->file('featured_img');
