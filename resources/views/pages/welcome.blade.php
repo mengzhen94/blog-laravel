0713d9dc5bcc7620c5aca55ee6c22717257803dc
Mengzhen <gao.mengzhen94@gmail.com>
2017-03-28 23:11:48 -0400

    fix popular post

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -9,7 +9,7 @@
           <div class="jumbotron">
             <h1>Welcome to My Blog!</h1>
             <p class="lead">Thank you so much for visiting. This is my test website built with Laravel. Please read my popular post!</p>
-            <p><a class="btn btn-primary btn-lg" href="#" role="button">Popular Post</a></p>
+            <p><a class="btn btn-primary btn-lg" href="{{ route('blog.single', 'image')}}" role="button">Popular Post</a></p>
           </div>
         </div>
       </div>
